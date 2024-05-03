5b2c67c480004eaf2c31739a4b08584aefb7b32e
to4to <justjustifying@outlook.com>
2024-05-03 23:36:20 +0530

    chore: Add ERC721 implementation and minting functionality to BasicNft contract

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -3,8 +3,24 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-contract BasicNft {
-    constructor() {
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+contract BasicNft  is ERC721{
+
+    uint256 public s_tokenCounter;
+    constructor() ERC721("Dogie", "DOG"){
+        s_tokenCounter = 0;
+    }
+
+
+    function mintNft()public  {
         
     }
+
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return "https://assets.wanderers.ai/file/wandernauts/metadata/2202";
+}
+
+
 }
