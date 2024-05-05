e149d002df9de457b6242da21acf92a15b2077b9
to4to <justjustifying@outlook.com>
2024-05-06 00:52:49 +0530

    chore: Add BasicNftTest contract and dependencies

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -1,5 +1,16 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
+import{Test } from "forge-std/Test.sol";    
+import {BasicNft} from "../src/BasicNft.sol";
+import{DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 
+contract BasicNftTest is Test{
+DeployBasicNft deployer;
+BasicNft basicNft;
 
+function setUp() public   {
+
+}
+
+}
