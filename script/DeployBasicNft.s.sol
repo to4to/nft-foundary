bad8a88320486afb00c58438dac9006c3ead0d9c
to4to <justjustifying@outlook.com>
2024-05-05 23:37:04 +0530

    broadcast

---
+++ script/DeployBasicNft.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+
+import {Script } from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+contract DeployBasicNft is Script{
+
+
+function run() external returns (BasicNft){
+vm.startBroadcast( );
+BasicNft basicNft = new BasicNft(); 
+vm.stopBroadcast();
+
+}
+
+}
