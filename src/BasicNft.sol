a79816762c6b20867c12aaddd3bdcaaf49c23cad
to4to <justjustifying@outlook.com>
2024-05-05 23:31:38 +0530

    chore: Refactor BasicNft contract to support minting and tokenURI retrieval

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -1,26 +1,28 @@
-
-
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-contract BasicNft  is ERC721{
 
+contract BasicNft is ERC721 {
     uint256 public s_tokenCounter;
-    constructor() ERC721("Dogie", "DOG"){
+    mapping(uint256 => string) private s_tokenIdToUri;
+
+    constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
+    function mintNft(string memory tokenURI) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenURI;
 
-    function mintNft()public  {
-        
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
     }
 
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        // return "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
-}
-
-
+        return s_tokenIdToUri[tokenId];
+    }
 }
